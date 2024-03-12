b97afa557dce02c85b89363187be333bb13f222a
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-12 19:13:32 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <div>
-     <h1 className="text-3xl font-bold underline">
+    <div className='bg-purple-500'>
+     <h1 className='text-xl'>
       Hello world!
     </h1>
+    <button className='btn'>Click</button>
     </div>
   );
 }
