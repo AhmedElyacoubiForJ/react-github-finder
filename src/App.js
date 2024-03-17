2fea2c232a5deb46bb6c3f932d4239888694759e
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-03-17 05:36:18 +0100

    Alert Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GithubProvider } from "./context/github/GithubContext";
 
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
@@ -17,6 +18,7 @@ function App() {
           <div className="flex flex-col justify-between h-screen">
             <NavBar />
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
